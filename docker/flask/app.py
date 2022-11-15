98a173c0732e828c1a33fc8fba002c6aa42fe163
its-lautaro <lautaro.lavecchia@hotmail.com>
2022-11-14 21:00:29 -0300

    update flask app. add template + mqtt client

--- docker/flask/app.py
+++ docker/flask/app.py
@@ -1,14 +1,15 @@
 # compose_flask/app.py
 from flask import Flask
 from redis import Redis
+from flask import render_template
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-def hello():
-    return"<p>Hello, maite!</p>"
-
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
