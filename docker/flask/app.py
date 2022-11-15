43e3c33bb2eed41133e013ccf04517fae5b11c54
its-lautaro <lautaro.lavecchia@hotmail.com>
2022-11-14 21:45:01 -0300

    add mqtt client and ui

--- docker/flask/app.py
+++ docker/flask/app.py
@@ -1,15 +1,35 @@
-# compose_flask/app.py
 from flask import Flask
 from redis import Redis
+import paho.mqtt.client as mqtt
 from flask import render_template
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis',port=6379)
+
+topic = 'test'
+
+def on_connect(client, userdata, flags, rc):
+    client.publish(topic, "Cliente conectado. -Flask")
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+@app.route('/walk', methods=['GET', 'POST'])
+def walk():
+   client.publish(topic,"walk")
+   return (''), 204
 
+@app.route('/dance', methods=['GET', 'POST'])
+def dance():
+   client.publish(topic,"dance")
+   return (''), 204
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.username_pw_set('Flask', 'DefaultFlask')
+    client.on_connect = on_connect
+    client.connect('172.18.0.2')
+    client.loop_start()
+
+    app.run(host='0.0.0.0', port=5000)
